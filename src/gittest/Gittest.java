828b9e8f516aaa453d8fb73ae6253aedf69594c7
Chan Thongjerm <chan.thongjerm@mail.kmutt.ac.th>
2020-10-20 17:42:24 +0700

    Feature2

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -16,10 +16,20 @@ public class Gittest {
      */
     public static void main(String[] args) {
         sayHello();
+        sayHi();
+        sayGoodbye();
     }
     
     public static void sayHello() {
         System.out.println("Hello");
+    }
+    
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+    
+    public static void sayGoodbye() {
+        System.out.println("Goodbye");
     }
     
 }
